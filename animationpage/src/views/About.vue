ae5d4865e33abf9e2aa4237769633b0a05c43b87
bilgekosee <76417507+bilgekosee@users.noreply.github.com>
2022-12-22 12:45:19 +0300

    animation-page

---
+++ animationpage/src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <img src="https://picsum.photos/800/300" alt="Placeholder" />
+    <h1>About</h1>
+    <p>
+      Kendinizi Değiştirin ve Geliştirin! "Yenilik, liderle takipçiyi
+      birbirinden ayırır." Kendinizi ve işinizi yenilemeniz gerekiyor. Gelişmeye
+      ve ilerlemeye devam edin. Eğer değişiklik yapmıyorsanız, başkaları sizi
+      ele geçirecek ve işten çıkarılacaksınız. Sizin yaptığınız işin aynısını
+      yapan milyarlarca insan var, peki onlardan farkınız ne? Yaratıcı ve
+      yenilikçi olmanın bir yolunu düşünmelisiniz! Steve Jobs hiçbir zaman takip
+      etmedi, kendi yeniliğinin ve icadının olduğuna inanıyordu. Alexander
+      Graham Bell, telefonu icat ettiğinde, hiçbir zaman herhangi birisini takip
+      etmiyordu. Steve Jobs'a göre, insanlar, bir ürünü görene kadar o ürüne
+      ihtiyaç olup olmadığını asla bilemezler. Ve bu, bir lider ve bir takipçi
+      arasında ayrım yapan şeydir.
+    </p>
+    <p>
+      “Zamanınız sınırlıdır, bu yüzden başkalarının hayatını yaşayarak
+      harcamayın. Başka insanların düşünceleriyle yaşayan dogma tarafından
+      kapana kısılmayın. Başkalarının görüşlerinin gürültüsünün kendi iç
+      sesinizi dışarıda bırakmasına izin vermeyin. Ve en önemlisi, kalbinizi ve
+      sezginizi takip etme cesaretiniz olmalı.” Hayat kısadır ve başkalarının
+      hayatını yaşamak için harcamamalısınız. Başkalarının fikrinin senin
+      gerçeğin olmasına izin verme! Hayaliniz veya hedefleriniz yoksa mutlaka
+      bunlara sahip olun. Hayal etmeye ve kalbini takip etmeye cesaret et. Asla
+      başkalarına ne yapabileceğinizi veya yapamayacağınızı söylemeyin.
+      İnanılmaz başarılar elde eden tüm insanlar hayallerini takip ediyorlar.
+      Michael Jordan'a, Richard Branson'a, Elon Musk'a, Larry Ellison'a, Bill
+      Gates'e, Warren Buffett'e vb. Bakın, hepsi kalplerini ve hayallerini takip
+      ederler. Tutkuları üzerinde çalışırlar ve çalışmalarına hakim olurlar. Ve
+      sonunda, kendi alanlarındaki en iyiler haline geliyorlar. Yani şu anda
+      bunu yapıyor musunuz?
+    </p>
+  </main>
+</template>
---
+++ animationpage/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>
+      Gençlere, “Çevrenizde sizi zorlayacak, size yeni şeyler öğretecek ve daha
+      ileri gitmenizi sağlayacak insanların bulunmasını sağlayın” diyen Bill
+      Gates, yapay zekâ, enerji ve biyolojik bilim alanlarında yakın gelecekte
+      çok büyük önem kazanacağını ve bu alanda kariyer yapan gençlerin iş
+      dünyasında büyük rağbet göreceğini belirtti.
+    </p>
+    <p>
+      “Evrene bir iz bırakmak için buradayız. Olağanüstü başarı elde etmek söz
+      konusu olduğunda, büyük düşünen insanlardan söz etmeliyiz. Steve Jobs
+      büyük düşünen ve dünyadaki izini bırakmak isteyen kişidir. İnanılmazdı ve
+      her zaman olağanüstü bir şey yapmak istiyordu, benzersiz bir şey, devrim,
+      dünyayı değiştirecek, evrene iz bırakacak bir şey... Ve yaptı. Steve Jobs
+      başka bir sözünde de şöyle diyor: “Dünyayı değiştirebileceklerini
+      düşünecek kadar çılgınca olanlar, yapanlardır.” Önce kendi hayallerine
+      inanmalısın. İstediğinizi elde edebileceğinize inanmıyorsanız, bunu
+      yapmanın hiçbir yolu yoktur. Dolayısıyla, hayal etmeye ve çılgınca
+      düşünmeye cesaret edin. Diğer insanlar, deli olduğunuzu ve imkansız
+      olduğunu söylerlerse söylesinler. Sen sensin ve bu senin hayatın, nasıl
+      yaşamak istediğini ve neyi başarmak istediğini sadece sen seçersin.
+      Kendinize güvenin ve hayallerinize inanın. Evrene bir iz bırakmaya hazır
+      mısınız?
+    </p>
+  </main>
+</template>
